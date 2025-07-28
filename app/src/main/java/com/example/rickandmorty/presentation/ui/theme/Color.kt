bcab23c0e14d08b32ceb1de9699a110fe7dc5a0a
veliCosanay <96071908+veliCosanay@users.noreply.github.com>
2025-07-29 02:54:09 +0300

    moved to presentation package

--- app/src/main/java/com/example/rickandmorty/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.rickandmorty.ui.theme
+package com.example.rickandmorty.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/rickandmorty/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/ui/theme/Theme.kt
@@ -1,6 +1,6 @@
-package com.example.rickandmorty.ui.theme
+package com.example.rickandmorty.presentation.ui.theme
 
-import android.app.Activity
+
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
